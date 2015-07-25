fe80b834c8375d70b4a1f047f6f2784d5a8478fd
George Novik <grgnvk@gmail.com>
2015-07-26 00:02:50 +0300

    First commit

---
+++ SoilAckServiceBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fliak
+ * Date: 30.5.15
+ * Time: 8.07
+ */
+
+namespace Soil\AckServiceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SoilAckServiceBundle extends Bundle  {
+
+}
